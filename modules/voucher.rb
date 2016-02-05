41561825291ca4748db61d3a6bfec7da9d45cbd5
Fabian Franz <fabian.franz@students.fh-hagenberg.at>
2016-02-05 13:56:38 +0100

    some ideas by @DokuKaefer

--- modules/voucher.rb
+++ modules/voucher.rb
@@ -15,6 +15,23 @@ class VoucherCreator < JPanel
     @output_format = JComboBox.new @output_formats
     @output_format_label = JLabel.new "Ausgabeformat"
     
+    @time_range = {
+      "eine Stunde" => 3600,
+      "zwei Stunden" => 7200,
+      "vier Stunden" => 14400,
+      "acht Stunden" => 28800,
+      "ein Tag" => 86400,
+      "zwei Tage" => 172800,
+      "drei Tage" => 259200,
+      "vier Tage" => 345600,
+      "fünf Tage" => 432000,
+      "sechs Tage" => 518400,
+      "eine Woche" => 604800,
+      "zwei Wochen" => 1209600
+    }
+    @validity = JComboBox.new @time_range.keys.to_java
+    @validity_label = JLabel.new "Gültigkeitsdauer"
+    
     @voucher_providers = ::OPNsense::Voucher.list_providers.to_java
     @voucher_provider = JComboBox.new @voucher_providers
     @voucher_provider_label = JLabel.new "Voucher Provoider"
@@ -25,6 +42,7 @@ class VoucherCreator < JPanel
     form = [
         @amount_of_vouchers_label,@amount_of_vouchers,
         @roll_name_label,@roll_name,
+        @validity_label, @validity,
         @output_format_label,@output_format,
         @voucher_provider_label, @voucher_provider,
         @empty_label, @button
@@ -44,7 +62,7 @@ class VoucherCreator < JPanel
         f.write render_template(format, download_voucher)
       end
       system('"%s" "%s"' % [$configuration['browser'],path])
-      File.delete(path)
+      #File.delete(path)
     end
     
     if tabbar
@@ -53,7 +71,7 @@ class VoucherCreator < JPanel
   end
   
   def download_voucher
-    ::OPNsense::Voucher.create! @voucher_provider.get_selected_item, @amount_of_vouchers.value, 10, @roll_name.text
+    ::OPNsense::Voucher.create! @voucher_provider.get_selected_item, @amount_of_vouchers.value, @time_range[@validity.get_selected_item], @roll_name.text
   end
 
   def render_template(template, voucher)
@@ -62,6 +80,6 @@ class VoucherCreator < JPanel
     return rendered_template
   end
 end
-$windowsizes << java.awt.Dimension.new(550, 200)
+$windowsizes << java.awt.Dimension.new(550, 220)
 VoucherCreator.new($tab_pane)
 
